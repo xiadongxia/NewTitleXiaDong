51dce93ff78813adf1afdb50d108ace8d0eda90e
夏冬boy <451266738@qq.com>
2017-02-09 18:33:36 +0800

    添加--log

--- app/src/main/java/items/bwei/com/newtitlexiadong/MainActivity.java
+++ app/src/main/java/items/bwei/com/newtitlexiadong/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("log","我是夏冬----");
+        Log.d("log","我是夏冬----我是夏冬");
     }
 }
