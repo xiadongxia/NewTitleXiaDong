9ebd80770e76dd55828603bbd70b2f678a3f6374
夏冬boy <451266738@qq.com>
2017-02-09 18:26:47 +0800

    首次

---
+++ app/src/main/java/items/bwei/com/newtitlexiadong/MainActivity.java
@@ -0,0 +1,13 @@
+package items.bwei.com.newtitlexiadong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
