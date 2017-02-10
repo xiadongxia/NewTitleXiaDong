69d0218c27f2a61f0455fa72882932f533e7b7b3
夏冬boy <451266738@qq.com>
2017-02-10 11:54:06 +0800

    首页底部4个RadioButton+4个fragment

--- app/src/main/java/items/bwei/com/newtitlexiadong/MainActivity.java
+++ app/src/main/java/items/bwei/com/newtitlexiadong/MainActivity.java
@@ -1,15 +1,70 @@
 package items.bwei.com.newtitlexiadong;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import fragment.Fragment_attention;
+import fragment.Fragment_home;
+import fragment.Fragment_my;
+import fragment.Fragment_video;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout fl_main_fragment;
+    RadioGroup rg_main_radiogroup_buttom;
+    RadioButton home;
+    RadioButton video;
+    RadioButton attention;
+    RadioButton my;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("log","夏冬----我是夏冬");
+
+        fl_main_fragment = (FrameLayout)findViewById(R.id.fl_main_fragment);
+        rg_main_radiogroup_buttom = (RadioGroup)findViewById(R.id.rg_main_radiogroup_buttom);
+        home = (RadioButton)findViewById(R.id.home);
+        video = (RadioButton)findViewById(R.id.video);
+        attention = (RadioButton)findViewById(R.id.attention);
+        my = (RadioButton)findViewById(R.id.my);
+
+
+        rg_main_radiogroup_buttom.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.home:
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_fragment,new Fragment_home()).commit();
+
+                        break;
+
+                    case R.id.video:
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_fragment,new Fragment_video()).commit();
+
+                        break;
+
+                    case R.id.attention:
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_fragment,new Fragment_attention()).commit();
+
+                        break;
+
+                    case R.id.my:
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_fragment,new Fragment_my()).commit();
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
